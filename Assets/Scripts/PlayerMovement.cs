e985efe6a332fef6da4eac09a804b61c0b5a39ac
Daniel <danielcardosot17@gmail.com>
2022-10-05 22:47:14 -0300

    Forward movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField][Range(0.5f, 10f)] private float moveSpeed = 5;
     [SerializeField][Range(0.5f, 10f)] private float rotationSpeed = 5;
-    // Start is called before the first frame update
-    void Start()
+
+
+
+    private InputActions inputActions;
+    private InputAction move;
+
+    private void Awake()
     {
-        
+        inputActions = new InputActions();
+        move = inputActions.Player.Move;
     }
+
+    private void OnEnable()
+    {
+        inputActions.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Disable();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveKeyboard();
+
+    }
+    private void MoveKeyboard()
+    {
+        var direction = move.ReadValue<Vector2>();
+        if(direction.y > 0)
+        {
+            MoveShipForward(direction.y);
+        }
+        if(direction.x != 0)
+        {
+            RotateShip(direction.x);
+        }
+    }
+
+    private void RotateShip(float rotationDirection)
+    {
+    }
+
+    private void MoveShipForward(float forwardDirection)
+    {
+        transform.position += moveSpeed * Time.deltaTime * forwardDirection * transform.up;
     }
 }
