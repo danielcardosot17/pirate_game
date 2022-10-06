67ceffd2370938f07f39133b864e672a120a9c02
Daniel <danielcardosot17@gmail.com>
2022-10-05 21:47:07 -0300

    menu manager and enemyspawner and reference to respective sliders for option menu

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,10 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField][Range(0.5f, 10f)] private float spawnTime = 5;
+    const float MIN_SPAWN_TIME = 1f;
+    const float MAX_SPAWN_TIME = 10f;
+
+    [SerializeField][Range(1f, 10f)] private float spawnTime = 5;
     [SerializeField][Range(1, 10)] private int shooterSpawnCount = 3;
     [SerializeField][Range(1, 10)] private int chaserSpawnCount = 3;
+
+    [SerializeField] private Slider spawnTimeSlider;
+    [SerializeField] private TMP_Text spawnNumberText;
+
+
+
+
+
+
+    public void ChangeTime()
+    {
+        spawnTime = spawnTimeSlider.value * (MAX_SPAWN_TIME - MIN_SPAWN_TIME) + MIN_SPAWN_TIME;
+    }
+    public void ChangeNumberText()
+    {
+        spawnNumberText.text = spawnTime.ToString("N1");
+    }
 }
--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -1,21 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameMaster : MonoBehaviour
 {
+    const float MIN_SESSION_TIME = 60f;
+    const float MAX_SESSION_TIME = 180f;
+
     [SerializeField][Range(60f, 180f)] private float sessionTimeInSeconds = 60;
+
     [SerializeField] private Slider sessionTimeSlider;
-    [SerializeField] private TMP_Text sessionTimeText;
+    [SerializeField] private TMP_Text sessionNumberText;
+
 
 
     private AudioManager audioManager;
+    private MenuManager menuManager;
 
     private void Awake()
     {
         audioManager = FindObjectOfType<AudioManager>();
+        menuManager = FindObjectOfType<MenuManager>();
     }
     void Start()
     {
@@ -26,5 +31,16 @@ public class GameMaster : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+
+    public void ChangeTime()
+    {
+        sessionTimeInSeconds = sessionTimeSlider.value * (MAX_SESSION_TIME - MIN_SESSION_TIME) + MIN_SESSION_TIME;
+    }
+    public void ChangeNumberText()
+    {
+        sessionNumberText.text = (sessionTimeInSeconds/60f).ToString("N1");
     }
 }
