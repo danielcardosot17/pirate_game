d0268f50f6586ef5e67962eb329ae15dfb4c27fe
Daniel <danielcardosot17@gmail.com>
2022-10-06 14:47:50 -0300

    Menu UI button navigation flow

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -11,25 +11,65 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private GameObject endgameCanvas;
     [SerializeField] private GameObject optionsCanvas;
     [SerializeField] private GameObject pauseCanvas;
+    [SerializeField] private GameObject duringGameCanvas;
+
+
+    private void Awake()
+    {
+        HideStartgameCanvas();
+        HideEndgameCanvas();
+        HideOptionsCanvas();
+        HidePauseCanvas();
+        HideDuringGameCanvas();
+    }
+
+    public void HidePauseCanvas()
+    {
+        pauseCanvas.SetActive(false);
+    }
+    public void HideDuringGameCanvas()
+    {
+        duringGameCanvas.SetActive(false);
+    }
+
+    public void HideOptionsCanvas()
+    {
+        optionsCanvas.SetActive(false);
+    }
+
+    public void HideEndgameCanvas()
+    {
+        endgameCanvas.SetActive(false);
+    }
+
+    public void HideStartgameCanvas()
+    {
+        startgameCanvas.SetActive(false);
+    }
 
     public void ShowEndgameCanvas()
     {
-
+        endgameCanvas.SetActive(true);
     }
 
     public void ShowStartgameCanvas()
     {
-
+        startgameCanvas.SetActive(true);
     }
 
     public void ShowPauseCanvas()
     {
-
+        pauseCanvas.SetActive(true);
     }
 
     public void ShowOptionsCanvas()
     {
+        optionsCanvas.SetActive(true);
+    }
 
+    public void ShowDuringGameCanvas()
+    {
+        duringGameCanvas.SetActive(true);
     }
 
 }
